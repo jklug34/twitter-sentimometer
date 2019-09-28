e7bf47bf888b324126fa3c6c9c20dbe2ee463b8b
chienyuanhung <chien.yuanhung@gmail.com>
2019-09-28 10:05:56 -0700

    add bar.js

--- js/histogram.js
+++ js/histogram.js
@@ -15,7 +15,7 @@ d3.json("data/iphone_tweets_score.json", function(error, data){
         new_score = 2*(+d.score-0.5)
         iphoneScores.push(new_score)
     });
-     console.log(iphoneScores)
+    // console.log(iphoneScores)
     // read galaxy data
     d3.json("data/galaxy_tweets_score.json", function(error, data){
         var galaxyScores = []
@@ -44,14 +44,56 @@ d3.json("data/iphone_tweets_score.json", function(error, data){
         var x = d3.scaleLinear()
               .domain([-1,1])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
               .range([0, width]);
+
         svg.append("g")
               .attr("transform", "translate(0," + height + ")")
               .call(d3.axisBottom(x));
 
         var histogram = d3.histogram()
-              .value(function(d) { return +d.value; })   // I need to give the vector of value
               .domain(x.domain())  // then the domain of the graphic
               .thresholds(x.ticks(40)); // then the numbers of bins
+
+        var bins1 = histogram(iphoneScores);
+        var bins2 = histogram(galaxyScores);
+
+        var y = d3.scaleLinear()
+          .range([height, 0]);
+          y.domain([0, d3.max(bins1, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+       
+        svg.append("g")
+          .call(d3.axisLeft(y));
+
+        svg.selectAll("rect")
+          .data(bins1)
+          .enter()
+          .append("rect")
+            .attr("x", 1)
+            .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+            .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+            .attr("height", function(d) { return height - y(d.length); })
+            .style("fill", "#69b3a2")
+            .style("opacity", 0.4)
+        
+        svg.selectAll("rect2")
+            .data(bins2)
+            .enter()
+            .append("rect")
+              .attr("x", 1)
+              .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+              .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+              .attr("height", function(d) { return height - y(d.length); })
+              .style("fill", "#F2DA57")
+              .style("opacity", 0.4)
+
+
+        svg.append("circle").attr("cx",300).attr("cy",30).attr("r", 6).style("fill", "#69b3a2")
+        svg.append("circle").attr("cx",300).attr("cy",60).attr("r", 6).style("fill", "#F2DA57")
+        svg.append("text").attr("x", 320).attr("y", 30).text("Apple iphone").style("font-size", "15px").attr("alignment-baseline","middle")
+        svg.append("text").attr("x", 320).attr("y", 60).text("Samsung Galaxy").style("font-size", "15px").attr("alignment-baseline","middle")
+
+        
+
+            
 
     })  
 })
